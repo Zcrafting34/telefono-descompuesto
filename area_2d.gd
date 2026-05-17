01e58196f14d7c75e70f41d9d4b603d12d82ca31
Yaxhid_Ryus <yaxhidfnaf2811@gmail.com>
2026-05-16 21:56:25 -0300

    parkoursinfinal

---
+++ area_2d.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass
+	#me quede sin tiempo, ponele aca que cuando toque al body con name player pase a otra escena de victoria
--- player.gd
+++ player.gd
@@ -4,17 +4,22 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+var jumps_left = 2
+
 
 func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		$AnimatedSprite2D.play("fall")
 		velocity += get_gravity() * delta
+	else:
+		jumps_left = 2
 
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumps_left > 0:
 		$jump.play()
 		velocity.y = JUMP_VELOCITY
+		jumps_left -= 1
 
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
@@ -34,3 +39,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+
+#Me encontre el juego sin jugabilidad, solo un pj que se mueve, entonces como no vi la manera de poner enemigos
+#ni nada parecido vi bien que sea un juego de parkour, subis y llegas a la meta
